a344620b52d5158a3c8ccadc6e5aa8cc69d454af
sofia <romata1966@gmail.com>
2021-04-28 22:12:00 +0200

    Final

--- Roll-a-Ball/Assets/Source/PlayerController.cs
+++ Roll-a-Ball/Assets/Source/PlayerController.cs
@@ -2,28 +2,38 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
-
+using TMPro;
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 0;
 
-    private Rigidbody rb;
+    // Create public variables for player speed, and for the Text UI game objects
+    public float speed;
+    public TextMeshProUGUI countText;
+    public GameObject winTextObject;
 
     private float movementX;
     private float movementY;
+
+    private Rigidbody rb;
+    private int count;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+
+        // Set the text property of the Win Text UI to an empty string, making the 'You Win' (game over message) blank
+        winTextObject.SetActive(false);
     }
 
-    private void OnMove(InputValue movementValue)
+    void OnMove(InputValue value)
     {
-        Vector2 movementVector = movementValue.Get<Vector2>();
+        Vector2 v = value.Get<Vector2>();
 
-        movementX = movementVector.x;
-        movementY = movementVector.y;
+        movementX = v.x;
+        movementY = v.y;
     }
 
     private void FixedUpdate()
@@ -32,5 +42,24 @@ public class PlayerController : MonoBehaviour
 
         rb.AddForce(movement * speed);
     }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Cube"))
+        {
+            other.gameObject.SetActive(false);
+            count += 1;
+            // Run the 'SetCountText()' function (see below)
+            SetCountText();
+        }
+    }
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
 
+        if (count >= 8)
+        {
+            // Set the text value of your 'winText'
+            winTextObject.SetActive(true);
+        }
+    }
 }
